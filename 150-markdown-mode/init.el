2bb1ea9a93ed0ee8a8ed84b856b09d20c5ee930d
Zachary Pinter <zpinter@gmail.com>
2008-12-28 15:14:39 -0700

    Adding support for markdown

---
+++ 150-markdown-mode/init.el
@@ -0,0 +1,8 @@
+(add-module-path "markdown")
+
+(autoload 'markdown-mode "markdown-mode.el"
+  "Major mode for editing Markdown files" t)
+
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mkdn$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.text$" . markdown-mode))
