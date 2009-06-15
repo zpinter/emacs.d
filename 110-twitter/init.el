605c20ab077aa27823e4fddd7c921d00c6b1b817
Zachary Pinter <zpinter@gmail.com>
2009-06-14 18:13:29 -0600

    Switched to twit.el

--- 110-twitter/init.el
+++ 110-twitter/init.el
@@ -1,4 +1,19 @@
-(add-module-path "twittering")
-(require 'twittering-mode)
+(add-module-path "lisp")
+(require 'twit)
 
-(setq twittering-icon-mode t)
+;; (load "~/my-elisp/twit.el")
+;;(add-module-path "twittering")
+;;(require 'twittering-mode)
+;;(setq twittering-icon-mode t)
+(defface twit-title-face
+    '((((background light))
+       (:background "PowderBlue"
+        :underline "DeepSkyBlue"
+        :box (:line-width 2 :color "PowderBlue" :style 0)))
+      (((background dark))
+       (:background "PowderBlue"
+        :underline "DeepSkyBlue"
+        :box (:line-width 2 :color "PowderBlue" :style 0)))
+      (t (:underline "black")))
+  "Title Area of the recent tweets buffer."
+  :group 'twit)
