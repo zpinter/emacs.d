1831e5266c2ca2bf9299040f3ed6b1d27e50b4a0
Zachary Pinter <zpinter@gmail.com>
2009-06-15 20:17:38 -0600

    Added an extra tweak to magit for staging everything, even untracked

--- 100-magit/init.el
+++ 100-magit/init.el
@@ -2,3 +2,9 @@
 (require 'magit)
 
 (defalias 'gs 'magit-status)
+
+(defun magit-stage-all-even-untracked ()
+  (interactive)
+  (magit-run-git "add" "."))
+
+(defalias 'ga. 'magit-stage-all-even-untracked)
