4e5857f95cb3bd711009ec77d07dd0c9d8d99f9e
Zachary Pinter <zpinter@gmail.com>
2010-12-19 13:28:21 -0700

    Tweaking to use built-in cedet

--- cedet/init.el
+++ cedet/init.el
@@ -1,4 +1,3 @@
-(load-file (concat zconfig-current-module-dir "/cedet-1.0pre6/common/cedet.el"))
-(global-ede-mode 1)
-(semantic-load-enable-minimum-features)
-(semantic-load-enable-code-helpers)
+(global-ede-mode t)
+;(semantic-load-enable-minimum-features)
+;(semantic-load-enable-code-helpers)
