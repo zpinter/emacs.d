de5ad4a14d1ae6aab9dfad27ae19d88fedee0a39
Zachary Pinter <zpinter@gmail.com>
2010-12-24 13:33:36 -0700

    More ibuffer tweaks

--- ibuffer/init.el
+++ ibuffer/init.el
@@ -1,32 +1,36 @@
 (require 'ibuffer)
-(setq ibuffer-saved-filter-groups
-  (quote (("default"      
-            ("Org" ;; all org-related buffers
-              (mode . org-mode))  
-            ("Term"
-				 (or
-              (mode . term-mode)
-              (mode . term-char-mode)))
-            ("Zinio"
-				 (or
-              (filename . "org/zinio/")				  
-              (filename . "repos/zinio/")
-              (filename . "projects/zinio/")))
-            ("Zebra"
-				 (or
-              (filename . "org/zebra/")				  				  
-              (filename . "repos/zebra")
-              (filename . "projects/zebra")))
-            ("Programming" ;; prog stuff not already in MyProjectX
-              (or
-                (mode . c-mode)
-                (mode . perl-mode)
-                (mode . python-mode)
-                (mode . emacs-lisp-mode)
-                (mode . js2-mode)					 
-                ;; etc
-                )) 
-            ("Git"   (mode . magit-mode))))))
+
+(setq ibuffer-default-sorting-mode 'major-mode)
+
+;; (setq ibuffer-saved-filter-groups
+;;   (quote (("default"      
+;;             ("Org" ;; all org-related buffers
+;;               (mode . org-mode))  
+;;             ("Term"
+;; 				 (or
+;;               (mode . term-mode)
+;;               (mode . term-char-mode)))
+;;             ("Zinio"
+;; 				 (or
+;;               (filename . "org/zinio/")				  
+;;               (filename . "repos/zinio/")
+;;               (filename . "projects/zinio/")))
+;;             ("Zebra"
+;; 				 (or
+;;               (filename . "org/zebra/")				  				  
+;;               (filename . "repos/zebra")
+;;               (filename . "projects/zebra")))
+;;             ("Programming" ;; prog stuff not already in MyProjectX
+;;               (or
+;;                 (mode . c-mode)
+;;                 (mode . perl-mode)
+;;                 (mode . python-mode)
+;;                 (mode . emacs-lisp-mode)
+;;                 (mode . js2-mode)					 
+;;                 ;; etc
+;;                 )) 
+;;             ("Git"   (mode . magit-mode))))))
+
 (global-set-key (kbd "C-x b") 'ibuffer)
 (add-hook 'ibuffer-mode-hook
   (lambda ()
