2c114ab1c8fe7e2889fc778993a4a004f802bf45
Zachary Pinter <zachary.pinter@hulu.com>
2012-10-13 14:50:36 -0700

    Autoloading of haml files

--- haml-mode/init.el
+++ haml-mode/init.el
@@ -1,4 +1,6 @@
-(require 'haml-mode)
+(autoload 'haml-mode "haml-mode" "Major mode for editing haml files." t)
+(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+;; (require 'haml-mode)
 (add-hook 'haml-mode-hook
 			 '(lambda ()
 				 (setq indent-tabs-mode nil)
