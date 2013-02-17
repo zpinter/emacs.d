17ad5dd4a024b9a33e51d1b911ef67691cda9a3b
Zachary Pinter <zachary.pinter@hulu.com>
2013-02-17 10:11:53 -0800

    Quicker file nav

--- tweaks/init.el
+++ tweaks/init.el
@@ -117,6 +117,24 @@
 
 (transient-mark-mode t)
 
+;; quickly go home/.emacs.d in repos
+(add-hook 'ido-setup-hook
+ (lambda ()
+   ;; Go straight home
+   (define-key ido-file-completion-map
+     (kbd "~")
+     (lambda ()
+		 (interactive)
+		 (if (looking-back "~/")
+			  (insert ".emacs.d/")
+			(if (looking-back "/")
+				 (insert "~/")
+			  (call-interactively 'self-insert-command)))))))
+
+
+;; (if (looking-back "~/")
+;; (insert ".emacs.d/")
+;; (if PREVIOUS-IF)) 
 ;(setq default-major-mode 'org-mode)
 
 (add-to-list 'auto-mode-alist '("\\.el$" . lisp-mode))
