893861ee0e911d231a347247a2f47b69c65bb8c5
Zachary Pinter <zachary.pinter@hulu.com>
2013-02-23 11:08:31 -0800

    auto-complete tweaks

--- auto-complete/init.el
+++ auto-complete/init.el
@@ -1,6 +1,19 @@
 (require 'auto-complete-config)
 
 (add-to-list 'ac-dictionary-directories (concat zconfig-current-module-dir "/lisp/dict"))
+
+
+
 (ac-config-default)
+;; equivalent to...
+;; (defun ac-config-default ()
+;;   (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
+;;   (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+;;   (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+;;   (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+;;   (add-hook 'css-mode-hook 'ac-css-mode-setup)
+;;   (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+;;   (global-auto-complete-mode t))
+
 
 (global-auto-complete-mode t)
