f5fb6ce41d1b54ce44cf051f05a62f088b15f82c
Zachary Pinter <zachary.pinter@hulu.com>
2013-02-23 11:09:58 -0800

    Switched to solaried-theme repo with better support for emacsclient

--- solarized-theme/update.el
+++ solarized-theme/update.el
@@ -1,14 +1,4 @@
-(let ((lisp-dir (concat zconfig-current-module-dir "/lisp"))
-		(update-dir (concat zconfig-current-module-dir "/update")))
+;; (zconfig-update-from-git-simple "emacs-color-theme-solarized" "git://github.com/sellout/emacs-color-theme-solarized.git")
 
-  (shell-command (concat "rm -rf " lisp-dir "/*"))
-  (shell-command (concat "rm -rf " update-dir "/emacs-color-theme-solarized"))
-
-  (let ((build-cmd (concat "cd " update-dir " && git clone https://github.com/sellout/emacs-color-theme-solarized.git emacs-color-theme-solarized"
-			  " && cd emacs-color-theme-solarized"
-			  " && rm -rf .git"
-			  " && cp -r ./* ../../lisp/")))
-
-	 (message build-cmd)
-	 (shell-command build-cmd))
-)
+;prefer the emacsclient patches from this branch for now
+(zconfig-update-from-git-simple "emacs-color-theme-solarized" "git://github.com/mwolson/emacs-color-theme-solarized")
