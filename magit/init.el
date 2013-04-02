f6f6f2d7966af3194c0e918f8d8107a6d810a692
Zachary Pinter <zachary.pinter@hulu.com>
2013-04-01 21:08:55 -0700

    magit tweaks (mainly windows-related)

--- magit/init.el
+++ magit/init.el
@@ -5,11 +5,24 @@
 		(setenv "PATH" (concat (getenv "PATH") ":~/homebrew/bin"))
 		(setq exec-path (append exec-path '("~/homebrew/bin")))))
 
+
+;; (when (iswindows)
+;;   (let ((gitpath (expand-file-name "~/Dropbox/windows/portablegit/bin")))
+;; 	 (let ((gitcmd (concat gitpath "/git.exe")))
+;; 		(when (file-exists-p gitcmd)
+;; 		  (setq exec-path (remove "/usr/bin" exec-path))
+;; 		  (setq exec-path (remove gitpath exec-path))
+;; 		  (add-to-list 'exec-path "/usr/bin")
+;; 		  (add-to-list 'exec-path gitpath)
+;; 		  (setenv "PATH" (concat "/usr/bin;" gitpath ";" (getenv "PATH")))
+;; 		  (setq magit-git-executable gitcmd)))))
+
 (defalias 'gs 'magit-status)
 
 (defun magit-stage-all-even-untracked ()
   (interactive)
-  (magit-run-git "add" "."))
+  (magit-run-git "add" ".")
+  (magit-run-git "add" "-u"))
 
 (defalias 'ga. 'magit-stage-all-even-untracked)
 
@@ -33,4 +46,6 @@
 	  ;; (defun magit-diff-U-arg ()
 	  ;; 	 (format "-U%dw" magit-diff-context-lines))
 	  (add-to-list 'magit-diff-options "-w")
+	  ;;don't seem to use the magit-mark-item binding at all
+	  (define-key magit-status-mode-map (kbd ".") 'magit-stage-all-even-untracked)
 	  ))
