cb819e816cb3d1e55ef1bf0ba8db5b57433585db
Zachary Pinter <zachary.pinter@hulu.com>
2013-04-01 21:08:30 -0700

    js2-mode fix

--- js2-mode/init.el
+++ js2-mode/init.el
@@ -1,4 +1,4 @@
-(autoload 'js2-mode "js2" nil t)
+(autoload 'js2-mode "js2-mode" nil t)
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
