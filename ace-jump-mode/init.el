8adc13722a349b8887feb8f19f9ae40c6d30b610
Zachary Pinter <zachary.pinter@hulu.com>
2013-04-01 21:04:04 -0700

    Change ace-jump-mode keybinding

--- ace-jump-mode/init.el
+++ ace-jump-mode/init.el
@@ -1,3 +1,3 @@
 (autoload 'ace-jump-mode "ace-jump-mode" nil t)
 ;; (require 'ace-jump-mode)
-(define-key global-map (kbd "C-c C-SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-,") 'ace-jump-mode)
