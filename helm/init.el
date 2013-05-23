214990c5edf63761cd6dd61885b23c0ebfabd3d4
Zachary Pinter <zachary.pinter@hulu.com>
2013-05-23 11:17:44 -0700

    Helm keybinding for google search

--- helm/init.el
+++ helm/init.el
@@ -1,6 +1,7 @@
 (require 'helm-config)
 
 (global-set-key (kbd "C-c C-h") 'helm-mini)
+(global-set-key (kbd "C-c d") 'helm-google-suggest)
 
 (eval-after-load "helm"
   '(set-face-background 'helm-selection "Black"))
