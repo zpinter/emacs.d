8b7e75c8ff150e4f38432bde13a01af8e52f0f08
Zachary Pinter <zachary@grandtrunklabs.com>
2013-12-08 12:39:21 -0800

    Emacs jedi config

--- jedi-config.el
+++ jedi-config.el
@@ -1,3 +1,27 @@
-(add-hook 'python-mode-hook 'jedi:setup)
+(defun z:find-parent-dir (dir-to-find)
+  (progn
+      (defun find-parent-dir-r (dir-to-find path)
+         "find the dir from the parent directories"
+         (let* ((parent (file-name-directory path))
+                (possible-dir (concat parent dir-to-find)))
+           (cond
+             ((file-exists-p possible-dir) (throw 'found-it possible-dir))
+             ((string= (concat "/" dir-to-find) possible-dir) nil)
+             (t (find-parent-dir-r dir-to-find (directory-file-name parent))))))
+
+    (if (buffer-file-name)
+        (catch 'found-it
+          (find-parent-dir-r dir-to-find (buffer-file-name)))
+        nil)))
+
+(defun z:jedi:setup ()
+  (let ((venv-dir (z:find-parent-dir "venv")))
+    (if venv-dir
+        (setq jedi:server-args `("--virtual-env" ,venv-dir "--sys-path" ,venv-dir)))
+    )
+  (jedi:setup)
+)
+
+(add-hook 'python-mode-hook 'z:jedi:setup)
 (setq jedi:setup-keys t)                      ; optional
 (setq jedi:complete-on-dot t)                 ; optional
