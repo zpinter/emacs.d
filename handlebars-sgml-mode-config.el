8a96654d561d96a761bb54f2df322546c0ce82c0
Zachary Pinter <zachary@grandtrunklabs.com>
2013-12-08 11:42:19 -0800

    handlebars file types

--- handlebars-sgml-mode-config.el
+++ handlebars-sgml-mode-config.el
@@ -1,3 +1,4 @@
 (add-to-list 'auto-mode-alist '("\\.html\\'" . sgml-mode))
+(add-to-list 'auto-mode-alist '("\\.handlebars\\'" . sgml-mode))
 (require 'handlebars-sgml-mode)
 (handlebars-use-mode 'global)
